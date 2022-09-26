b0d5f8d33bc3a826564365931efa21b85adaf15e
Bahatisos <lumumbasospeter1@gmail.com>
2022-09-26 14:43:11 -0700

    8 print

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,12 +10,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, l = 0;
+	int i, j, p, l = 0, r = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		p = (i *size) + i;
-		I += *(a + p);
+		l += *(a + p);
 	}
 	for (j = 0; j < size; j++)
 	{
